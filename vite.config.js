48d771810a7d84b7886b1ea297be3b35155bc491
Michaela Beladova <misunka@Michaelas-MacBook-Air.local>
2026-05-06 21:56:16 +0200

    chore: cleanup after merge

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
-  base: '/canadian-medical/', // https://github.com/misunka/canadian-medical
-
+  base: '/canadian-medical/', // MUSÍ tam být tato lomítka
 })
-=======
-  base: '/canadian-medical/', 
-})
->>>>>>> cc0c9c4cb3b9af651e065eff468007b1921d14eb
